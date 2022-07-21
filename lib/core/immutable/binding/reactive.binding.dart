7481927a0c13763da814be89c6017d815925bb80
Abdulgani Al-Monifi <abdulgani.almonifi@gmail.com>
2022-07-21 19:37:49 +0300

    [Bug Fixes] Fix: error on stream listener when listens to the same listener more than once

--- lib/core/immutable/binding/reactive.binding.dart
+++ lib/core/immutable/binding/reactive.binding.dart
@@ -3,10 +3,13 @@ import 'dart:async';
 class WingsReactive<T> {
   final StreamController<T> _controller = StreamController<T>();
 
+  late final Stream<T>? _stream;
+
   T? _data;
 
   WingsReactive(T data) {
     _data = data;
+    _stream = _controller.stream.asBroadcastStream();
 
     _controller.sink.add(_data as T);
   }
@@ -19,7 +22,7 @@ class WingsReactive<T> {
 
   T get data => _data!;
 
-  Stream<T> get stream => _controller.stream;
+  Stream<T> get stream => _stream!;
 }
 
 extension Wis<T> on T {
